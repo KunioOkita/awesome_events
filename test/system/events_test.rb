be488ff6e4f3e87ca45fc8becbe5f1744a7b776e
hs_okita.kunio <okita.kunio@heartsoft.co.jp>
2021-07-23 15:31:05 +0900

    簡単なシステムテストを実装

---
+++ test/system/events_test.rb
@@ -0,0 +1,18 @@
+# coding: utf-8
+require "application_system_test_case"
+
+class EventsTest < ApplicationSystemTestCase
+  test "/events/:id ページを表示する" do
+    event = FactoryBot.create(:event)
+    visit event_url(event)
+    assert_selector "h1", text: event.name
+  end
+
+  test "/events/new ページを表示する" do
+    sign_in_as(FactoryBot.create(:user))
+    visit new_event_url
+
+    assert_selector "h1", text: "イベント作成"
+  end
+
+end
